f57bf4b1c8565da1a3e741f72a5c76286074c7f7
Tony Wong <twong@psu.edu>
2018-06-28 06:40:40 -0600

    new preliminary optimization template, and trying MCMC with sig18+GLAC+LIFE

--- R/GEOCARB_preliminary_optimization.R
+++ R/GEOCARB_preliminary_optimization.R
@@ -172,130 +172,33 @@ source('run_geocarbF.R')
 # need the likelihood function and prior distributions
 source('GEOCARB-2014_calib_likelihood.R')
 
-# set up and run the actual calibration
-# interpolate between lots of parameters and one parameter.
-# this functional form yields an acceptance rate of about 25% for as few as 10
-# parameters, 44% for a single parameter (or Metropolis-within-Gibbs sampler),
-# and 0.234 for infinite number of parameters, using accept_mcmc_few=0.44 and
-# accept_mcmc_many=0.234.
-accept_mcmc_few <- 0.44         # optimal for only one parameter
-accept_mcmc_many <- 0.234       # optimal for many parameters
-accept_mcmc <- accept_mcmc_many + (accept_mcmc_few - accept_mcmc_many)/length(parnames_calib)
-niter_mcmc <- niter_mcmc000
-gamma_mcmc <- 0.5
-stopadapt_mcmc <- round(niter_mcmc*1.0)# stop adapting after ?? iterations? (niter*1 => don't stop)
+# set up and run the actual optimization
+
+
+
 
 ##==============================================================================
-## Actually run the calibration
-if(n_node000==1) {
-  tbeg=proc.time()
-  amcmc_out1 = MCMC(log_post, n=niter_mcmc, init=par_calib0, adapt=TRUE, acc.rate=accept_mcmc,
-                  scale=step_mcmc, gamma=gamma_mcmc, list=TRUE, n.start=max(5000,round(0.05*niter_mcmc)),
-                  par_fixed=par_fixed0, parnames_calib=parnames_calib,
-                  parnames_fixed=parnames_fixed, age=age, ageN=ageN,
-                  ind_const_calib=ind_const_calib, ind_time_calib=ind_time_calib,
-                  ind_const_fixed=ind_const_fixed, ind_time_fixed=ind_time_fixed,
-                  input=input, time_arrays=time_arrays, bounds_calib=bounds_calib,
-                  data_calib=data_calib, ind_mod2obs=ind_mod2obs,
-                  ind_expected_time=ind_expected_time, ind_expected_const=ind_expected_const,
-                  iteration_threshold=iteration_threshold)
-  tend=proc.time()
-  chain1 = amcmc_out1$samples
-} else if(n_node000 > 1) {
-  # TODO -- add support for MCMC.parallel
-}
+## Actually run the optimization
+tbeg=proc.time()
+amcmc_out1 = MCMC(log_post, n=niter_mcmc, init=par_calib0, adapt=TRUE, acc.rate=accept_mcmc,
+                scale=step_mcmc, gamma=gamma_mcmc, list=TRUE, n.start=max(5000,round(0.05*niter_mcmc)),
+                par_fixed=par_fixed0, parnames_calib=parnames_calib,
+                parnames_fixed=parnames_fixed, age=age, ageN=ageN,
+                ind_const_calib=ind_const_calib, ind_time_calib=ind_time_calib,
+                ind_const_fixed=ind_const_fixed, ind_time_fixed=ind_time_fixed,
+                input=input, time_arrays=time_arrays, bounds_calib=bounds_calib,
+                data_calib=data_calib, ind_mod2obs=ind_mod2obs,
+                ind_expected_time=ind_expected_time, ind_expected_const=ind_expected_const,
+                iteration_threshold=iteration_threshold)
+tend=proc.time()
+chain1 = amcmc_out1$samples
 print(paste('Took ',(tend-tbeg)[3]/60,' minutes', sep=''))
 
-if(FALSE) {
-par(mfrow=c(3,2))
-for (p in 1:3) {
-  plot(chain1[,p], type='l', ylab=parnames_calib[p], xlab='Iteration')
-  hist(chain1[,p], xlab=parnames_calib[p], ylab='freq', main='')
-}
-}
+
 
 # save
 if(l_write_rdata) {
   save.image(file=paste(output_dir,'GEOCARB_MCMC_',appen,'_',today,'.RData', sep=''))
-}
-
-## Extend an MCMC chain?
-## Extend and run more MCMC samples?
-if(FALSE){
-niter_extend <- 1e4
-tbeg=proc.time()
-amcmc_extend1 = MCMC.add.samples(amcmc_out1, niter_extend,
-                                par_fixed=par_fixed0, parnames_calib=parnames_calib,
-                                parnames_fixed=parnames_fixed, age=age, ageN=ageN,
-                                ind_const_calib=ind_const_calib, ind_time_calib=ind_time_calib,
-                                ind_const_fixed=ind_const_fixed, ind_time_fixed=ind_time_fixed,
-                                input=input, time_arrays=time_arrays, bounds_calib=bounds_calib,
-                                data_calib=data_calib, ind_mod2obs=ind_mod2obs,
-                                ind_expected_time=ind_expected_time, ind_expected_const=ind_expected_const,
-                                iteration_threshold=iteration_threshold)
-tend=proc.time()
-chain1 = amcmc_extend1$samples
-}
-##==============================================================================
-
-
-##==============================================================================
-## Convergence diagnostics
-##========================
-
-# visual inspection
-
-#TONY TODO
-#TONY TODO
-# for now only look at climate sensitivity
-#ind_cs <- match('deltaT2X',parnames_calib)
-#plot(chain1[,ind_cs], type='l')
-#par(mfrow=c(7,8))
-#for (p in 1:length(parnames_calib)) {plot(chain1[,p], type='l', ylab=parnames_calib[p])}
-
-
-#TODO
-# Gelman and Rubin
-# Heidelberger and Welch
-# visual inspection
-
-# which parameters is climate sensitivity?
-ics <- which(parnames_calib=='deltaT2X')
-plot(chain1[,ics], type='l')
-
-parameters.posterior <- chain1
-covjump <- amcmc_out1$cov.jump
-
-##==============================================================================
-
-
-##==============================================================================
-## Write calibrated parameters output file
-##========================================
-
-## Get maximum length of parameter name, for width of array to write to netcdf
-## this code will write an n.parameters (rows) x n.ensemble (columns) netcdf file
-## to get back into the shape BRICK expects, just transpose it
-if(l_write_netcdf) {
-
-lmax=0
-for (i in 1:length(parnames_calib)){lmax=max(lmax,nchar(parnames_calib[i]))}
-
-## Name the file
-filename.parameters = paste('geocarb_calibratedParameters_',appen,'_',today,'.nc',sep="")
-
-dim.parameters <- ncdim_def('n.parameters', '', 1:ncol(parameters.posterior), unlim=FALSE)
-dim.name <- ncdim_def('name.len', '', 1:lmax, unlim=FALSE)
-dim.ensemble <- ncdim_def('n.ensemble', 'ensemble member', 1:nrow(parameters.posterior), unlim=TRUE)
-parameters.var <- ncvar_def('geocarb_parameters', '', list(dim.parameters,dim.ensemble), -999)
-parnames.var <- ncvar_def('parnames', '', list(dim.name,dim.parameters), prec='char')
-covjump.var <- ncvar_def('covjump', '', list(dim.parameters,dim.parameters), -999)
-outnc <- nc_create(filename.parameters, list(parameters.var,parnames.var, covjump.var))
-ncvar_put(outnc, parameters.var, t(parameters.posterior))
-ncvar_put(outnc, parnames.var, parnames_calib)
-ncvar_put(outnc, covjump.var, covjump)
-nc_close(outnc)
-
 }
 ##==============================================================================
 
