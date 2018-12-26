fef4495b6edb3f16560745552c53da83afe53b08
Tony Wong <twong@psu.edu>
2018-12-25 22:53:11 -0500

    adding codes to support Lenton et al degassing time series, and installingrelevant packages

---
+++ R/install_packages.R
@@ -0,0 +1,12 @@
+#===============================================================================
+# install packages that we need
+#===============================================================================
+
+install.packages('adaptMCMC')
+install.packages('ncdf4')
+install.packages('sn')
+install.packages('invgamma')
+install.packages('lhs')
+install.packages('DEoptim')
+install.packages('gdata')
+install.packages('Hmisc')
