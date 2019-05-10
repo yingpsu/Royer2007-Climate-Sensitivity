8583982c1410adbac208ba86739add4e20f65655
Tony Wong <twong@psu.edu>
2019-05-10 07:11:36 -0600

    initialization file creation for failed_run fixes

--- R/make_initialization_files.R
+++ R/make_initialization_files.R
@@ -1,21 +1,17 @@
 #===============================================================================
-# jump_covar_create.R
+# make_initialization_files.R
 #
-# Create transition covariance matrix for initializing Markov chains better.
+# Create transition covariance matrix and parameters vector for initializing
+# Markov chains better.
 #
 # Tony Wong (anthony.e.wong@colorado.edu)
 #===============================================================================
 
 setwd('./codes/GEOCARB/R')
 
-#load('../output/GEOCARB_MCMC_unc-sd10_29Jan2019sn.RData')  # first time
-load('../output/geocarb_mcmcoutput_unc_04Apr2019sn.RData') # third time, based on second
+load('../output/geocarb_mcmcoutput_unc_09May2019sn.RData') # based on standard parameters
 cov.jump <- amcmc_out1$cov.jump
 p0 <- amcmc_out1$samples[nrow(amcmc_out1$samples),]
-
-#load('../output/geocarb_mcmcoutput_unc_25Mar2019sn.RData') # second time, based on first
-#cov.jump <- amcmc_par1[[1]]cov.jump
-#p0 <- amcmc_par1[[1]]$samples[nrow(amcmc_par1[[1]]$samples),]
 
 appen <- 'unc-sd10'
 today <- Sys.Date(); today <- format(today,format="%d%b%Y")
